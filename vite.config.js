88751f083bc02e192df82b9551383ea2680b5112
Jon <jonwatkins86@gmail.com>
2023-04-03 02:47:25 +0100

    new build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig(() => {
   return {
-    //base: "/suika-example",
+    base: "/suika-example",
     build: {
       outDir: "./docs",
     },
